37f333996cdaf107b1ba76c15d32691a90683699
Deyvison Borges <35538358+deeborges@users.noreply.github.com>
2020-02-28 16:54:17 -0300

    Update UserController.ts

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -8,10 +8,7 @@ export class UserController extends BaseController<User>{
   }
 
   async save(request: Request) {
-    let _user = <User>request.body; // aqui faço um casting, tornando os dados do body em dados do User
-
-    // estou herdando de dentro do BaseController, as notifications
-    // passando as informações da orm do User para jogar entao pra baseController fazer as operações necessárias
+    let _user = <User>request.body; 
     super.IsRequired(_user.name, 'O nome do usuário é obrigatório');
     super.IsRequired(_user.photo, 'A foto do usuário é obrigatória');
     super.IsRequired(_user.email, 'O email do usuário é obrigatório');
